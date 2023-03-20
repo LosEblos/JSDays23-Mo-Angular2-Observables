7716162be70988b688de879331ccf9d707aed4eb
LosEblos <128365827+LosEblos@users.noreply.github.com>
2023-03-20 14:49:10 +0100

    1 file modified

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Todo } from "../models/todo";
+import { Todo } from '../models/todo';
 
 @Component({
   selector: 'app-todo',
@@ -13,10 +13,33 @@ export class TodoComponent implements OnInit {
 
   colorToBind = 'blue';
 
-  constructor() {}
+  constructor() {
+    console.log('TodoComponent', 'constructor');
+  }
 
-  ngOnInit() {}
+  //
+  // Lifecycle Hook...
+  // Werden von Angular
+  //
+  // Gibt auch noch: ngOnChanges
+  ngOnInit() {
+    console.log('TodoComponent', 'Lifecycle Hook', 'ngOnInit()');
+  }
+  // Gibt auch noch: ngAfterContentInit()
+  // Gibt auch noch: ngAfterContentChecked()
+  ngAfterViewInit() {
+    console.log('TodoComponent', 'Lifecycle Hook', 'ngAfterViewInit()');
+  }
+  ngAfterViewChecked() {
+    console.log('TodoComponent', 'Lifecycle Hook', 'ngAfterViewChecked()');
+  }
+  ngOnDestroy() {
+    console.log('TodoComponent', 'Lifecycle Hook', 'ngOnDestroy()');
+  }
 
+  //
+  // Individuelles...
+  //
   markTodoAsDone(todo: Todo) {
     todo.done = !todo.done;
     this.done.emit(todo);
