823cb19738ff78dddd60a561b3a86602c2e236c7
LosEblos <128365827+LosEblos@users.noreply.github.com>
2023-03-20 14:29:52 +0100

    2 files modified

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -7,25 +7,32 @@ import { HttpClient } from '@angular/common/http';
 export class TodoService {
   private actionUrl = 'https://tt-todos.azurewebsites.net/todos';
 
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(private readonly httpClient: HttpClient) {
+    console.log('TodoService', 'constructor');
+  }
 
   create(todo: Todo) {
+    console.log('TodoService', 'create', todo);
     return this.httpClient.post<Todo>(this.actionUrl, todo);
   }
 
   get(todoId: number) {
+    console.log('TodoService', 'get', todoId);
     return this.httpClient.get<Todo>(`${this.actionUrl}${todoId}`);
   }
 
   getAll(): Observable<Todo[]> {
+    console.log('TodoService', 'getAll');
     return this.httpClient.get<Todo[]>(this.actionUrl);
   }
 
   update(todo: Todo) {
+    console.log('TodoService', 'update', todo);
     return this.httpClient.put(`${this.actionUrl}${todo.id}`, todo);
   }
 
   delete(todoId: number) {
+    console.log('TodoService', 'delete', todoId);
     return this.httpClient.delete(`${this.actionUrl}${todoId}`);
   }
 }
